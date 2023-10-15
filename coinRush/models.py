9d3f54357b88f69056b42b32dbdede48d0a1c4f5
singhguri <singhguri5598@gmail.com>
2023-10-14 23:13:22 -0400

    added basic authentication

--- coinRush/models.py
+++ coinRush/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+
+from .managers import AuthUserManager
+
 
 # Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(default="", unique=True, blank=True, max_length=255)
+    name = models.CharField(max_length=255, blank=True, default="")
+
+    profile_pic = models.ImageField(null=True, blank=True)
+    photo_id = models.ImageField(null=True, blank=True)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    created_date = models.DateTimeField(default=timezone.now())
+    last_login = models.DateTimeField(blank=True, null=True)
+
+    objects = AuthUserManager()
+
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+    REQUERIES_FIELD = []
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name or self.email.split("@")[0]
--- coinRush/views.py
+++ coinRush/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home(request):
+    return render(request, "index.html")
+
+
+def signup(request):
+    return render(request, "registration/signup.html")
+
+
+def login(request):
+    return render(request, "registration/login.html")
+
+
+def logout(request):
+    return render(request, "index.html")
