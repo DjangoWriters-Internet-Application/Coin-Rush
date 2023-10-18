927d8d74dfb074c089d3d37dc1366345cc352a03
Ameya_Ade <ameyaaade@gmail.com>
2023-10-18 13:48:32 -0400

    Added basic model for News

--- coinRush/admin.py
+++ coinRush/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User,Stock,StockPrice,Transaction,UserHolding
+from .models import User,Stock,StockPrice,Transaction,UserHolding, News
 
 # Register your models here.
 admin.site.register(User)
@@ -8,3 +8,4 @@ admin.site.register(Stock)
 admin.site.register(StockPrice)
 admin.site.register(Transaction)
 admin.site.register(UserHolding)
+admin.site.register(News)
