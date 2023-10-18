374a9fb6c0b7f5f013e45f0d480d2a734952e8d5
Sahil Modi <sahilmodi@sahils-air.home>
2023-10-18 10:47:45 -0400

    Added model for uploading and showing stocks

--- coinRush/admin.py
+++ coinRush/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User,Stock,StockPrice
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Stock)
+admin.site.register(StockPrice)
--- coinRush/models.py
+++ coinRush/models.py
@@ -58,3 +58,24 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.created_by.username}"
+
+
+# Define a model for Stock
+class Stock(models.Model):
+    symbol = models.CharField(max_length=10, unique=True)
+    company_name = models.CharField(max_length=255)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2)
+    market_cap = models.DecimalField(max_digits=10, decimal_places=2)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.symbol
+
+# Define a model for StockPrice (to store historical price data)
+class StockPrice(models.Model):
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    date = models.DateField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.stock.symbol} - {self.date}"
