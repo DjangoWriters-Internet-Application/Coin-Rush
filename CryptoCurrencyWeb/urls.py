e252a7ba37569e72b2f35a24838fc03ba84fb892
Ameya_Ade <ameyaaade@gmail.com>
2023-10-25 12:51:02 -0400

    Added template for News

--- CryptoCurrencyWeb/urls.py
+++ CryptoCurrencyWeb/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from coinRush import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", TemplateView.as_view(template_name="index.html"), name="home"),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("news/", views.news, name='news')
     # path("", include("coinRush.urls")),
 ]
--- coinRush/views.py
+++ coinRush/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Transaction,UserHolding, User, News
+# from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
@@ -18,3 +19,10 @@ def login(request):
 def logout(request):
     return render(request, "index.html")
 
+
+def news(request):
+    context = {
+        'title': "Latest Crypto News",
+        'news':  News.objects.all()
+    }
+    return render(request, "News/index.html", context)
