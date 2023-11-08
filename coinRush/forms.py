1cb8fb477e90e92ca68f88991be84e29e2c6e477
Amitojasa <singh99amitoj@gmail.com>
2023-11-07 19:20:14 -0500

    added form for discussion

--- coinRush/forms.py
+++ coinRush/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import User
+from .models import User, Post
 from django.contrib.auth.forms import UserCreationForm
 
 
@@ -7,3 +7,20 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["name", "email", "profile_pic", "is_superuser"]
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+
+        widgets = {
+            'title': forms.TextInput(
+                attrs={'class': 'form-control', 'id': 'topicTitle', 'placeholder': 'Topic Title', 'required': 'true'}),
+            'content': forms.Textarea(
+                attrs={'class': 'form-control', 'id': 'topicContent', 'rows': '4', 'placeholder': 'Topic Content',
+                       'required': 'true'}),
+        }
+
+
+# class CommentForm
