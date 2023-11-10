f4b224d603615a10170d746dd1fae3303d91b063
AbhishekYawalkar21 <abhishekyawalkar4@gmail.com>
2023-11-09 19:30:59 -0500

    Updated templates for learn module

--- coinRush/models.py
+++ coinRush/models.py
@@ -129,7 +129,7 @@ class Learn(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     category = models.ForeignKey(CourseCategory, on_delete=models.CASCADE, default=1)
-    external_link = models.URLField(null=True)
+    # external_link = models.URLField(null=True)
 
     def __str__(self):
         return self.title
