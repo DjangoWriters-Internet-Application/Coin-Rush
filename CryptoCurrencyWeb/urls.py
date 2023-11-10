68d94be3fb585f07d37467587eb9c2279b124978
Sahil Modi <sahilmodi@sahils-air.home>
2023-11-10 15:05:04 -0500

    Added functionality to add image icon for currency

--- CryptoCurrencyWeb/urls.py
+++ CryptoCurrencyWeb/urls.py
@@ -17,9 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+
 from coinRush import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("coinRush.urls")),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
