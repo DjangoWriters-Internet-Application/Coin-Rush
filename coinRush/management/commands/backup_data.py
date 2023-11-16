4181dbd40fd4444e059565a2a654429e5b8d5995
AbhishekYawalkar21 <abhishekyawalkar4@gmail.com>
2023-11-16 14:56:28 -0500

    Updated backup and restore files

--- coinRush/management/commands/backup_data.py
+++ coinRush/management/commands/backup_data.py
@@ -1,4 +1,7 @@
+# your_app/management/commands/backup_data.py
+
 import json
+import os
 from django.core.management.base import BaseCommand
 from coinRush.models import CourseCategory, Learn
 
@@ -12,13 +15,29 @@ class Command(BaseCommand):
         course_categories = list(CourseCategory.objects.values())
 
         # Backup Learn
-        learns = list(Learn.objects.values())
+        learns = list(self.backup_learn())
 
         # Save the backup to a file
-        with open('learn_backup.json', 'w') as file:
+        with open('backup.json', 'w') as file:
             json.dump({
                 'course_categories': course_categories,
                 'learns': learns,
             }, file, indent=2)
 
         self.stdout.write(self.style.SUCCESS('Data backup complete'))
+
+    def backup_learn(self):
+        learns_data = []
+        for learn in Learn.objects.all():
+            # Get the relative path of the image within the app directory
+            image_relative_path = os.path.relpath(learn.image.path, 'coinRush/static/images/topic_images/')
+
+            learns_data.append({
+                'title': learn.title,
+                'description': learn.description,
+                'category': learn.category_id,
+                'slug': learn.slug,
+                'image': image_relative_path,  # Include relative image path
+            })
+
+        return learns_data
--- coinRush/management/commands/restore_data.py
+++ coinRush/management/commands/restore_data.py
@@ -1,6 +1,8 @@
 import json
 from django.core.management.base import BaseCommand
 from coinRush.models import CourseCategory, Learn
+from django.core.files.base import ContentFile
+import os
 
 class Command(BaseCommand):
     help = 'Restore the database data'
@@ -8,15 +10,15 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS('Restoring data...'))
 
-        # restore logic goes here
-        with open('learn_backup.json', 'r') as file:
+        # Your restore logic goes here
+        with open('backup.json', 'r') as file:
             backup_data = json.load(file)
 
         # Restore CourseCategory
         self.restore_model_data(CourseCategory, backup_data['course_categories'])
 
         # Restore Learn
-        self.restore_model_data(Learn, backup_data['learns'])
+        self.restore_learn_data(backup_data['learns'])
 
         self.stdout.write(self.style.SUCCESS('Data restore complete'))
 
@@ -28,3 +30,21 @@ class Command(BaseCommand):
         model.objects.bulk_create(
             model(**item) for item in data
         )
+
+    def restore_learn_data(self, learns_data):
+        for learn_data in learns_data:
+            # Get the full path to the image within the app directory
+            image_path = os.path.join('coinRush/static/images/topic_images/', learn_data.get('image', ''))
+
+            # Read the image file and save it to the Learn model
+            with open(image_path, 'rb') as image_file:
+                image_content = ContentFile(image_file.read(), name=os.path.basename(image_path))
+
+            # Restore Learn with image data
+            Learn.objects.create(
+                title=learn_data['title'],
+                description=learn_data['description'],
+                category_id=learn_data['category'],
+                slug=learn_data['slug'],
+                image=image_content,
+            )
